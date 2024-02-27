884d301e628c9dfc8285265e25481d9c1ba5e949
mansooriabbas <mansoori.abbas@outlook.com>
2024-02-28 00:07:35 +0100

    finished

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -3,11 +3,15 @@ import React, { useState } from "react";
 const ToDoList = () => {
   const [tasks, setTasks] = useState<{ text: string; done: boolean }[]>([]);
   const [newTask, setNewTask] = useState<string>("");
+  const [isEditing, setIsEditing] = useState<number | null>(null);
 
-  const handleInputChange = (e: {
-    target: { value: React.SetStateAction<string> };
-  }) => {
-    setNewTask(e.target.value);
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].text = e.target.value;
+    setTasks(updatedTasks);
   };
 
   const addTask = () => {
@@ -51,37 +55,60 @@ const ToDoList = () => {
     setTasks(updatedTasks);
   };
 
+  const editTask = (index: number) => {
+    setIsEditing((prevVal) => (prevVal === index ? null : index));
+  };
+
   return (
     <div className="to-do-list">
-      <h1>To-Do-List</h1>
-      <div>
+      <div className="title-container">
+        <h1>Task Master </h1>
+      </div>
+
+      <div className="input-container">
         <input
           type="text"
           placeholder="Enter a task..."
           value={newTask}
-          onChange={handleInputChange}
+          onChange={(e) => setNewTask(e.target.value)}
         />
         <button className="add-button" onClick={addTask}>
-          Add
+          <span className="material-symbols-outlined">add_task</span>
         </button>
       </div>
       <ol>
         {tasks.map((task, i) => (
           <li key={i}>
+            {isEditing === i && (
+              <input
+                onChange={(e) => handleInputChange(e, i)}
+                value={task.text}
+                type="text"
+              />
+            )}
+
             <span className={task.done ? "text done" : "text"}>
               {task.text}
             </span>
+
             <button className="delete-button" onClick={() => deleteTask(i)}>
-              Delete
+              <span className="material-symbols-outlined">delete</span>
             </button>
             <button className="move-button" onClick={() => moveTaskUp(i)}>
-              UP
+              <span className="material-symbols-outlined">arrow_upward</span>
             </button>
             <button className="move-button" onClick={() => moveTaskDown(i)}>
-              DOWN
+              <span className="material-symbols-outlined">arrow_downward</span>
             </button>
             <button className="mark-button" onClick={() => markTaskDone(i)}>
-              DONE
+              <span className="material-symbols-outlined">check</span>
+            </button>
+            <button className="edit-button" onClick={() => editTask(i)}>
+              {isEditing === i ? (
+                <span className="material-symbols-outlined">save</span>
+              ) : (
+                <span className="material-symbols-outlined">edit</span>
+              )}
             </button>
           </li>
         ))}
